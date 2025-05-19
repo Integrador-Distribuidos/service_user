ac56a7f9fd4f6cf5fc3ba3366c0b6cc10c1756ac
MarcosFelintoLN <marcos.leao@escolar.ifrn.edu.br>
2025-05-19 12:11:35 -0300

    criação do serviço de usuários

---
+++ users_service/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from users_service.users.models import User
+from users_service.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
