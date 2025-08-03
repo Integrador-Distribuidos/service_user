a000d02ac3e59873e0da913728cc6b0366044c49
MarcosFelintoLN <marcos.leao@escolar.ifrn.edu.br>
2025-08-03 13:42:58 -0300

    Adição de endereço padrão

--- users_service/addresses/models.py
+++ users_service/addresses/models.py
@@ -2,19 +2,25 @@ from django.db import models
 from users_service.users.models import User
 
 class Address(models.Model):
-    user = models.ForeignKey(User, related_name="addresses" ,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="addresses", on_delete=models.CASCADE)
     street = models.CharField(max_length=255, verbose_name="Rua")
-    number = models.CharField(max_length=255, verbose_name="Número")
+    number = models.CharField(max_length=255, verbose_name="Número")
     neighborhood = models.CharField(max_length=255, verbose_name="Bairro")
     city = models.CharField(max_length=255, verbose_name="Cidade")
     state = models.CharField(max_length=255, verbose_name="Estado")
     zip_code = models.CharField(max_length=10, verbose_name="CEP")
     uf = models.CharField(max_length=2, verbose_name="UF")
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    is_default = models.BooleanField(default=False, verbose_name="Endereço Padrão")
+
     class Meta:
-        verbose_name = "Endereço"
-        verbose_name_plural = "Endereços"
+        verbose_name = "Endereço"
+        verbose_name_plural = "Endereços"
 
     def __str__(self):
         return self.street
+
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            Address.objects.filter(user=self.user).exclude(id=self.id).update(is_default=False)
+        super().save(*args, **kwargs)
