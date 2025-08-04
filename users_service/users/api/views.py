12ef8c7d2a3dee4a2e38e1ac484f19bbab18ee68
MarcosFelintoLN <marcos.leao@escolar.ifrn.edu.br>
2025-08-03 22:40:11 -0300

    Virar adm

--- users_service/users/api/views.py
+++ users_service/users/api/views.py
@@ -22,3 +22,18 @@ class UserViewSet(viewsets.ModelViewSet):
     def me(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=["patch"], permission_classes=[permissions.IsAuthenticated])
+    def make_admin(self, request, pk=None):
+        user = self.get_object()
+
+        if user != request.user:
+            return Response({"detail": "Você não pode modificar o tipo de outro usuário."}, status=status.HTTP_403_FORBIDDEN)
+
+        if user.type != User.UserType.CLIENT:
+            return Response({"detail": "Este usuário já é admin."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.type = User.UserType.ADMIN
+        user.save()
+
+        return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
